5c05ebd62c6ee1ac9964893af86fb04ebba2fa66
HamzaEkram <37058173+HamzaEkram@users.noreply.github.com>
2018-03-05 11:24:57 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+function save(){
+alert("You clicked the right link");
+}
+function button(){
+alert("You clicked the right button");
+}
+var i;
+function greet(){
+ i= prompt("Please enter your name");
+alert("Hello,  " + i);
+}
+function greets(){
+ i= prompt("Please enter your name");
+ var len=i.length;
+alert("Hello, " + i + "\n" + "Length of user name is  " +  len );
+}
+
+function phone(){
+alert("Thanks for purchasing a phone from us");
+}
+function lImage(x){
+x.style.width="300px"
+x.style.height="300px"
+}
+function sImage(x){
+x.style.width="250px"
+x.style.height="250px"
+}
